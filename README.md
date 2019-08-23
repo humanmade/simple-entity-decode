f398ac8855a124b063df6acde2e24e3c65496f56
Ryan McCue <me@ryanmccue.info>
2019-08-23 13:22:32 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# Simple Entity Decode
+
+A tiny library to decodes HTML numeric entities, and the basic XML named entities.
+
+Most entity libraries include complex handling for entities, and include the full list of named entities from the HTML specification. However, most applications and pages don't need this, and can instead normalise entities (e.g. on the server).
+
+Specifically, this library decodes:
+
+* Basic named entities (`&amp;`, `&apos;`, `&quot;`, `&lt;`, `&gt;`)
+* Numeric decimal entities (`&#163;`)
+* Numeric hexadecimal entities (`&#x000A3;`)
+
+It does **not** handle other named entities.
+
+## Licence
+
+Copyright 2019 Human Made. Licensed under the [MIT license](LICENSE.txt).
+
+Uses code from [he by Mathias Bynens](https://github.com/mathiasbynens/he). Used under the MIT license.
---
+++ index.js
@@ -0,0 +1,59 @@
+const ENTITY_REGEX = /&(?:#(?:([0-9]+)|x([a-fA-F0-9]+))|(amp|apos|quot|lt|gt));/g;
+const NAMED_ENTITIES = {
+	amp: '&',
+	apos: "'",
+	quot: '"',
+	lt: '<',
+	gt: '>',
+};
+
+/**
+ * Decode numeric entities in text.
+ *
+ * Decodes numeric (decimal and hexadecimal) entities into their Unicode
+ * representation as a string.
+ *
+ * This is a much more lightweight decoder than is needed for generic HTML
+ * handling, as we control the server-side representation.
+ *
+ * @param {string} text
+ * @return {string}
+ */
+export function decodeEntities( text ) {
+	return text.replace(
+		ENTITY_REGEX,
+		( _, decimal, hex, named ) => {
+			if ( named ) {
+				return NAMED_ENTITIES[ named ];
+			}
+
+			const codePoint = decimal ? parseInt( decimal, 10 ): parseInt( hex, 16 );
+			return codePointToSymbol( codePoint );
+		}
+	);
+}
+
+/**
+ * Convert a code point to the Unicode symbol it represents.
+ *
+ * From https://github.com/mathiasbynens/he
+ *
+ * @param {Number} codePoint Unicode code point for a symbol
+ * @return {string} Symbol represented by the code point
+ */
+export function codePointToSymbol( codePoint ) {
+	let output = '';
+	if ( ( codePoint >= 0xD800 && codePoint <= 0xDFFF ) || codePoint > 0x10FFFF ) {
+		// “Otherwise, if the number is in the range 0xD800 to 0xDFFF or is
+		// greater than 0x10FFFF, then this is a parse error. Return a U+FFFD
+		// REPLACEMENT CHARACTER.”
+		return '\uFFFD';
+	}
+	if ( codePoint > 0xFFFF ) {
+		codePoint -= 0x10000;
+		output += String.fromCharCode( ( ( codePoint >>> 10 ) & 0x3FF ) | 0xD800 );
+		codePoint = ( 0xDC00 | codePoint ) & 0x3FF;
+	}
+	output += String.fromCharCode( codePoint );
+	return output;
+}
